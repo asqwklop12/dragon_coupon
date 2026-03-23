30d8ae7b83eced7372c89117c520e9a641b5ecd7
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2026-03-23 20:33:42 +0900

    kafka like 구조 반영

--- infra/kafka/src/main/java/com/dragons/consumer/CouponIssueRequestedEventConsumer.java
+++ infra/kafka/src/main/java/com/dragons/consumer/CouponIssueRequestedEventConsumer.java
@@ -3,9 +3,14 @@ package com.dragons.consumer;
 import com.dragons.coupon.issue.CouponIssueRequestHandler;
 import com.dragons.coupon.issue.CouponIssueRequestedEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.BackOff;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CouponIssueRequestedEventConsumer {
@@ -14,10 +19,18 @@ public class CouponIssueRequestedEventConsumer {
 
   private final CouponIssueRequestHandler couponIssueRequestHandler;
 
+  @RetryableTopic(
+      backOff = @BackOff(delay = 1000, multiplier = 2.0)
+  )
   @KafkaListener(topics = COUPON_ISSUE_REQUEST_TOPIC,
       groupId = "coupon-issue-consumer-group",
       concurrency = "7")
   public void handle(CouponIssueRequestedEvent event) {
     couponIssueRequestHandler.handle(event);
+  }
+
+  @DltHandler
+  public void dltHandle(CouponIssueRequestedEvent event) {
+    log.error("DLT 이동 eventId={}", event.eventId());
   }
 }
